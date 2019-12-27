658c9d0e2b306a677e674b210e60eee804aea372
AntonioNisini <nisant.mail@gmail.com>
2019-12-27 23:52:18 +0100

    Test con VS Code

--- LED_Strip.ino
+++ LED_Strip.ino
@@ -1,3 +1,26 @@
 // LED_Strip
 //
 // Ciao Ale !!!
+
+extern HardwareSerial Serial;
+
+int led = LED_BUILTIN;
+
+void setup() {
+
+  Serial.begin(250000);
+  while (!Serial) ;
+  Serial.println("ciao");
+
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+
+  digitalWrite(led, HIGH);
+  delay(250);
+
+  digitalWrite(led, LOW);
+  delay(250);
+}
